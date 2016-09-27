a0821e259fc833ba6e31be5984fb1adb38662f36
Nicholas Ho <nicholas_hodj@outlook.com>
2016-09-27 10:48:03 +1000

    Added Json Media Type Formatter to accept Json types.

---
+++ ServerExperiment/Utils/JsonFhirFormatter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Hl7.Fhir.Serialization;
+
+namespace ServerExperiment.Utils
+{
+    public class JsonFhirFormatter : BufferedMediaTypeFormatter
+    {
+        public JsonFhirFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json+fhir"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            bool canRead = false;
+
+            // for single product object
+            if (type == typeof(Hl7.Fhir.Model.Patient) ||
+                type == typeof(Hl7.Fhir.Model.Device)  ||
+                type == typeof(Hl7.Fhir.Model.Observation))
+            {
+                canRead = true;
+
+                return canRead;
+            }
+            return canRead;
+            /*
+            else // Else block likely not going to be used.
+            {
+                // for multiple product objects
+                Type _type = typeof(IEnumerable<Hl7.Fhir.Model.Patient>);
+                return _type.IsAssignableFrom(type);
+            }
+            */
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            bool canWrite = false;
+
+            //for single product object
+            if (type == typeof(Hl7.Fhir.Model.Patient) ||
+                type == typeof(Hl7.Fhir.Model.Device) ||
+                type == typeof(Hl7.Fhir.Model.Observation))
+            {
+                canWrite = true;
+
+                return canWrite;
+            }
+            return canWrite;
+            /*
+            else // Else block likely not going to be used.
+            {
+                // for multiple product objects
+                Type _type = typeof(IEnumerable<Hl7.Fhir.Model.Patient>);
+                return _type.IsAssignableFrom(type);
+            }
+            */
+        }
+
+        public override void WriteToStream(Type type,
+                                    object value,
+                                    Stream writeStream,
+                                    HttpContent content)
+        {
+            using (StreamWriter writer = new StreamWriter(writeStream))
+            {
+                var resource = value as Hl7.Fhir.Model.Resource;
+                if (resource != null)
+                {
+                    string payload = FhirSerializer.SerializeResourceToJson(resource);
+
+                    writer.Write(payload);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Cannot serialize type");
+                }
+            }
+        }
+
+
+        public override object ReadFromStream(Type type,
+                                              Stream readStream,
+                                              HttpContent content,
+                                              IFormatterLogger formatterLogger)
+        {
+            string resourceString = string.Empty;
+            Hl7.Fhir.Model.Resource resource = null;
+
+            using (StreamReader reader = new StreamReader(readStream))
+            {
+                resourceString = reader.ReadToEnd();
+            }
+            
+            if (FhirParser.ProbeIsJson(resourceString))
+            {
+                resource = FhirParser.ParseResourceFromJson(resourceString) as Hl7.Fhir.Model.Resource;
+            }
+
+            return resource;
+        }
+    }
+}
